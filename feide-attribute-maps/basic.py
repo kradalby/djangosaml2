40ff22650f52dd83c45e32b2264c7c34492aaaae
Kristoffer Dalby <kradalby@kradalby.no>
2015-07-28 15:24:47 +0200

    added feide attribute maps

---
+++ feide-attribute-maps/basic.py
@@ -0,0 +1,20 @@
+
+MAP = {
+    "identifier": "urn:oasis:names:tc:SAML:2.0:attrname-format:basic",
+    "fro": {
+        'mail': 'mail',
+        'uid': 'uid',
+        'cn': 'cn',
+        'sn': 'sn',
+        'eduPersonPrincipalName': 'eduPersonPrincipalName',
+        'givenName': 'givenName',
+        },
+    "to": {
+        'mail': 'mail',
+        'uid': 'uid',
+        'cn': 'cn',
+        'sn': 'sn',
+        'eduPersonPrincipalName': 'eduPersonPrincipalName',
+        'givenName': 'givenName',
+    }
+}
---
+++ feide-attribute-maps/saml_uri.py
@@ -0,0 +1,22 @@
+__author__ = 'rolandh'
+
+
+MAP = {
+    "identifier": "urn:oasis:names:tc:SAML:2.0:attrname-format:uri",
+    "fro": {
+        'mail': 'mail',
+        'uid': 'uid',
+        'cn': 'cn',
+        'sn': 'sn',
+        'eduPersonPrincipalName': 'eduPersonPrincipalName',
+        'givenName': 'givenName',
+    },
+    "to": {
+        'mail': 'mail',
+        'uid': 'uid',
+        'cn': 'cn',
+        'sn': 'sn',
+        'eduPersonPrincipalName': 'eduPersonPrincipalName',
+        'givenName': 'givenName',
+    }
+}  
