868828a2618758b98d3146d0a50712bc893888e0
Kristoffer Dalby <kradalby@kradalby.no>
2015-08-11 08:13:47 +0200

    changed old django importlib to standard python

--- djangosaml2/conf.py
+++ djangosaml2/conf.py
@@ -15,9 +15,10 @@
 
 import copy
 
+from importlib import import_module
+
 from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
-from django.utils.importlib import import_module
 
 from saml2.config import SPConfig
 
